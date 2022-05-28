b97f6eb74c7599909f481ae471c8b6d2e21bd368
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-05-28 10:06:25 -0300

    Finished the basic configuration of the database and its models

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,32 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    ID:{
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false
+    }
+    ,
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    points: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    healthScore:{
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    instructions: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    }
   });
 };
