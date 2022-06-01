dc5e3b0094683c5b55ab11c05d766939dcdefe1d
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-06-01 16:59:09 -0300

    Finished the required routes and models for the backend

--- api/index.js
+++ api/index.js
@@ -19,10 +19,13 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-
+const populateDiets  = require('./src/utils/populateDiets.js');
+const populateDishes = require('./src/utils/populateDishes.js');
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
+    populateDiets()
+    populateDishes()
   });
 });
