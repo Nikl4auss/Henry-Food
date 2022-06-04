0e1ea51bbca3253465fe0995fca405e6d9ed2c66
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-06-04 16:22:37 -0300

    Finished basic React Routing configuration

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage';
+import NavBar from './components/NavBar/NavBar';
+import Home from './components/Home/Home';
+import Recipe from './components/Recipe/Recipe';
+import CreateRecipe from './components/CreateRecipe/CreateRecipe';
 function App() {
   return (
-    <div className="App">
-      <h1>Henry Food</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<LandingPage/>}/>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/recipe'>
+            <Route path='/recipe/:id' element={<Recipe />} />
+            <Route path='/recipe/create' element={<CreateRecipe />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
