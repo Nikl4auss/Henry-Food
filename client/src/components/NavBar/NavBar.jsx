e117103beaecdb5e520dfa552b8db76cf48f8909
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-06-06 19:35:43 -0300

    Finished the basic dsegin of the page navbar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import {NavLink, Outlet} from 'react-router-dom'
+import styles from './NavBar.module.css'
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <>
+      <nav className={styles.nav}>
+        <ul className={styles.nav_list}>
+          <li className={styles.nav_item}>
+            <NavButton path='/home' text={'Home'}/>
+          </li>
+          <li>
+            <NavButton path="/recipe/create" text={'Create Recipe'} />
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+}
+
+function NavButton({path, text}){
+  return (
+    <NavLink to={path} className={({isActive}) => isActive ? `${styles['nav_link']} ${styles['nav_link--active']} ` : styles['nav_link']}>
+      {text}
+    </NavLink>
   )
 }
 
