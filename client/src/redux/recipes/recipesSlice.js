ecc97b77a0e82b8b1c811b6fcf338b63dd7d9100
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-06-13 20:59:51 -0300

    Modified and simplified the redux configuration

--- client/src/redux/recipes/recipesSlice.js
+++ client/src/redux/recipes/recipesSlice.js
@@ -1,7 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
+import recipesServices from "../../services/recipesServices";
 
 const initialState = {
     recipes: [],
+    currentRecipe: {
+        id: "",
+        title: "",
+        summary: "",
+        points: 0,
+        healthScore: 0,
+        instructions: "",
+        image: "",
+        diets: [],
+        dishes: []
+    },
+    diets: [],
+    dishes: []
+
 }
 
 export const recipesSlice = createSlice({
@@ -14,10 +29,44 @@ export const recipesSlice = createSlice({
         ,
         addRecipe: (state, action) => {
             state.recipes.push(action.payload);
+        },
+        setCurrentRecipe: (state, action) => {
+            state.currentRecipe = action.payload;
+        },
+        setDiets: (state, action) => {
+            state.diets = action.payload;
+        },
+        setDishes: (state, action) => {
+            state.dishes = action.payload;
         }
     }
 }) 
 
-export const { setRecipes, addRecipe } = recipesSlice.actions;
+export const { setRecipes, addRecipe, setCurrentRecipe, setDiets, setDishes } = recipesSlice.actions;
+
+export function getRecipes(filters = {}) {
+    return async dispatch => {
+        try {
+            const recipes = await recipesServices.getAllRecipes(filters);
+            dispatch(setRecipes(recipes));
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+export function getDiets(){
+    return async dispatch => {
+        const diets = await recipesServices.getDiets();
+        dispatch(setDiets(diets));
+    }
+}
+
+export function getDishes(){
+    return async dispatch => {
+        const dishes = await recipesServices.getDishes();
+        dispatch(setDishes(dishes));
+    }
+}
 
 export default recipesSlice.reducer;
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import recipes from './recipes/recipesSlice'
-import recipe from './recipe/recipeSlice'
-import diets from './diets/dietsSlice'
-import dishes from './dishes/dishesSlice'
 export const store = configureStore({
     reducer: {
         recipes,
-        recipe,
-        diets,
-        dishes
     }
 })
