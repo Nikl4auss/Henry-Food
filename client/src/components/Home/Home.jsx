ff33a14fe2056cb77442beb374c71da47244a4bb
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-06-13 21:01:39 -0300

    Modified the Home component to show the Recipes container and Search Bar

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import {useEffect} from 'react'
 
+import Recipes from '../Recipes/Recipes'
+import {getRecipes, getDiets, getDishes} from '../../redux/recipes/recipesSlice'
+import SearchBar from '../SearchBar/SearchBar'
 function Home() {
+  const recipes = useSelector(state => state.recipes.recipes)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+      dispatch(getRecipes())
+      dispatch(getDiets())
+      dispatch(getDishes())
+  }, [dispatch])
   return (
-    <div>Home</div>
+    <main>
+      <h1>Welcome to the Henry Food App</h1>
+      <p>Search your favorites recipes</p>
+      <SearchBar />
+      <Recipes recipes={recipes}/>
+    </main>
   )
 }
 
