ff898f5adb073375e57abea76db8a8a6fb13aa66
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-06-13 21:05:18 -0300

    Modified the home component

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -6,7 +6,6 @@ import Recipes from '../Recipes/Recipes'
 import {getRecipes, getDiets, getDishes} from '../../redux/recipes/recipesSlice'
 import SearchBar from '../SearchBar/SearchBar'
 function Home() {
-  const recipes = useSelector(state => state.recipes.recipes)
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -19,7 +18,7 @@ function Home() {
       <h1>Welcome to the Henry Food App</h1>
       <p>Search your favorites recipes</p>
       <SearchBar />
-      <Recipes recipes={recipes}/>
+      <Recipes />
     </main>
   )
 }
