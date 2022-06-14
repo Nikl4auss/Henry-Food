01752f1a9444417a2d566c55052a346d34c41f98
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-06-13 21:03:12 -0300

    Added functions for interacting with the API

---
+++ client/src/services/recipesServices.js
@@ -0,0 +1,44 @@
+import axios from 'axios';
+
+const baseUrl = 'http://localhost:3001';
+
+async function getAllRecipes(filters = {}){
+    const filtersParams = Object.keys(filters)
+    try{
+        console.log('You are here')
+        const query = filtersParams.reduce((acc, key) => {
+            if(filters[key]){
+                if(Array.isArray(filters[key])){
+                    if(filters[key].length){
+                    return acc + `${key}=${filters[key].join(',')}&`;
+                    }
+                    return acc + ''
+                }
+                return acc + `${key}=${filters[key]}&`;
+            }
+            return acc + ''
+        }, '?')
+        const {data} = await axios.get(`${baseUrl}/recipes${query}`);
+        return data;
+        
+    }
+    catch(error){
+        console.log(error)
+    }
+}
+
+async function getDiets(){
+    const {data} = await axios.get(`${baseUrl}/types/diets`);
+    return data;
+}
+
+async function getDishes(){
+    const {data} = await axios.get(`${baseUrl}/types/dishes`);
+    return data;
+}
+
+export default {
+    getAllRecipes,
+    getDiets,
+    getDishes
+}
