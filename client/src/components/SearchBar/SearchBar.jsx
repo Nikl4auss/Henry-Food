0970e63eece3af13d067bf591a5f5756b6b723c6
Nicolas Joel Marinelli <nicolasmarinelli@protonmail.com>
2022-06-27 02:30:29 -0300

    Finished the Search Bar functionality with its styles

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -3,13 +3,17 @@ import {useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 
 import {getRecipes} from '../../redux/recipes/recipesSlice'
+import {ReactComponent as FilterIcon} from '../../assets/icons/filter-solid.svg'
+
+import styles from './SearchBar.module.css'
 
 function SearchBar() {
     const [filters, setFilters] = useState({
-        name: '',
+        title: '',
         diets: [],
         dishes: []
     })
+    const [showFilters, setShowFilters] = useState(false)
     const {diets, dishes} = useSelector(state => state.recipes)
     const dispatch = useDispatch()
 
@@ -27,27 +31,40 @@ function SearchBar() {
         dispatch(getRecipes(filters))
     }
   return (
-    <form onSubmit={searchRecipe}>
-        <input type="text" placeholder="Search" value={filters.name} onChange={(event) => setFilters({...filters, name: event.target.value})}/>
-        <div>
-          <p>Filter by:</p>
-          <div>
-            <p>Diet type</p>
-            {diets.map(diet => {
-              return <label key={diet}>{diet}<input type="checkbox" value={diet} name="diets" onChange={handleCheck}/></label>
-            })}
+    <form onSubmit={searchRecipe} className={styles.form__container}>
+        <div className={styles.form__searchbar_container}>
+          <input className={styles.form__searchbar_field} type="text" placeholder="Recipe Title" value={filters.title} onChange={(event) => setFilters({...filters, title: event.target.value})}/>
+          <button className={styles.form__searchbar_button} type="submit">Search</button>
+        </div>
+
+        <div className={styles.form__filter_container}>
+          <button className={styles.form__filter_button} onClick={() => showFilters ? setShowFilters(false) : setShowFilters(true)}>
+            Filter by
+            <FilterIcon className={styles.form__filter_icon}/>
+          </button>
+          <div className={`${styles.form__filter_groups} ${showFilters ? styles.form__filter_groups_active : ''}`}>
+            <div className={styles.form__filter_group}>
+              <p>Diet type</p>
+              <div>
+                {diets.map(diet => {
+                  return <label key={diet}><input type="checkbox" value={diet} name="diets" onChange={handleCheck}/> {diet}</label>
+                })}
+              </div>
+            </div>
+            <div className={`${styles.form__filter_group}`}>
+              <p>Dish type</p>
+              <div className={styles.form__filter_params}>
+                {dishes.map(dish => {
+                  return <label className={styles.form__filter_param} key={dish}><input type="checkbox" value={dish} name='dishes' onChange={handleCheck}/> {dish}</label>
+                })}
+              </div>
           </div>
-          <div>
-            <p>Dish type</p>
-            {dishes.map(dish => {
-              return <label key={dish}>{dish}<input type="checkbox" value={dish} name='dishes' onChange={handleCheck}/></label>
-            })}
+
           </div>
           <div>
 
           </div>
         </div>
-        <button type="submit">Search</button>
     </form>
   )
 }
